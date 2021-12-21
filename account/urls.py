a4e3f27f60cbf1db38a121e2795403430f99c510
malinastanciu <malina_stanciu@outlook.com>
2021-12-21 18:08:14 +0200

    Moved home in application

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from account import views as account_views
 
 urlpatterns = [
-    path('', account_views.home, name='home'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,1 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from arcgis.gis import GIS
-from IPython.display import display
 
-# Create your views here.
-def home(request):
-    gis = GIS()
-    map = gis.map("Palm Springs, CA")
-    return render(request, 'account/home.html', {'map': map})
