4e8602c273e770dfa0d652ad4a3460cee8ee632b
malinastanciu <malina_stanciu@outlook.com>
2021-12-21 19:19:37 +0200

    Created template for register and login

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from account import views as account_views
 
 urlpatterns = [
-
+    path('register/', account_views.registerPage, name='register'),
+    path('login/', account_views.loginPage, name='login'),
 ]
