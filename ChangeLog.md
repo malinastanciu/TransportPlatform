1173a2df19967a71597c20dcbf56783b3289c855
malinastanciu <malina_stanciu@outlook.com>
2021-12-22 17:53:47 +0200

    Created templetes for reset password

--- ChangeLog.md
+++ ChangeLog.md
@@ -4,6 +4,10 @@ Change Log for TransportPlatform
 What's new legend: [*] changed; [+] added; [-] fixed; [!] notes;
 
 ### dev
-- [+] [2h - Malina] - Created templates for login and register
+### 21.12.2021
+- [+] [2h - Malina] - Created templates for login and register 
 - [+] [2h - Malina] - Created the registration and authentication views depending on the type of user
 
+### 22.12.2021
+- [+] [1.5h - Malina] - Create templates for reset password
+- 
--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,21 @@
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from account import views as account_views
 
 urlpatterns = [
     path('register/', account_views.registerPage, name='register'),
     path('login/', account_views.loginPage, name='login'),
-    path('logout/', account_views.loginPage, name='login'),
+    path('logout/', account_views.logoutPage, name='logout'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'),
+         name="password_reset"),
+    path('reset_password_done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'),
+         name="password_reset_complete"),
 ]
